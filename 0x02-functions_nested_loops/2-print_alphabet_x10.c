baa45283fa4c0f010fc7362662f52f0bcdf2cb52
aliyu Abdulkabir <abdulkabiraliyu@gmail.com>
2023-03-21 03:28:39 -0700

    Printing a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,24 +2,29 @@
 /**
 * main: Entry point
 *
-* Description: This program prints the a-z in lower case by ustilizing _putchar function
+* Description: print_alphabet_x10  prints the a-z in lower case 10 times
 *
 * Return: Success (0)
 */
 
 int main(void)
 {
-	void print_alphabet(void)
+	void print_alphabet_x10(void)
 	{
+		int n;
 		char c;
+
+		for (n = 0; n <= 10; n++)
+		{
 
 		for (c = 'a'; c <= 'z' ; c++)
 			_putchar(c);
 
 
 		_putchar('\n');
-	}
 
+		}
+	}
 	return (0);
 }
 
