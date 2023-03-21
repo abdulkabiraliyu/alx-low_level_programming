e3f2c68e8a3cd4f536a29c6da5258ee85c230697
aliyu Abdulkabir <abdulkabiraliyu@gmail.com>
2023-03-21 04:12:41 -0700

    Print the sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_sign - print the sign of an input integer
+ *
+ * @n: takes integer type input from a function.
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + 48);
+		return (0);
+	}
+	else
+	{		_putchar(45 + 0);
+		return (-1);
+	}
+}
+
