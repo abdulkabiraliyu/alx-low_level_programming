4df726a7f0ddcf72d9d5a11b9c3911cb9ec6a4be
aliyu Abdulkabir <abdulkabiraliyu@gmail.com>
2023-03-21 03:08:08 -0700

    This task is to print a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* main: Entry point
+*
+* Description: This program prints the a-z in lower case by ustilizing _putchar function
+*
+* Return: Success (0)
+*/
+
+int main(void)
+{
+	void print_alphabets(void)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z' ; c++)
+			_putchar('c');
+
+
+		_putchar('\n');
+	}
+
+	return (0);
+}
+
