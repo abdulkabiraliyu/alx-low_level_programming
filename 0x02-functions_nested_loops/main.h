8c506a141f5a8223b104a698373e0dad0a72d3b3
aliyu Abdulkabir <abdulkabiraliyu@gmail.com>
2023-03-22 11:41:58 -0700

    main.h file edited and corrected

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,19 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include <stdio.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif /* MAIN_H */
