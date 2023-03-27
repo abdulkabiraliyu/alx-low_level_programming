fd3708ef7b0bac9f4df7739c2fb373766b5a69f7
aliyu Abdulkabir <abdulkabiraliyu@gmail.com>
2023-03-27 13:36:49 -0700

    some mistakes corrected

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,12 +10,12 @@ int main(void)
 	char password[PASSWORD_LENGTH+1];
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};:,.<>/?";
 
-    srand(time(NULL)); /* Seed the random number generator with the current time *\
+    srand(time(NULL)); /* Seed the random number generator with the current time */
 	for(int i = 0; i < PASSWORD_LENGTH; i++)
 	{
         password[i] = charset[rand() % (sizeof(charset) - 1)];
     }
-    password[PASSWORD_LENGTH] = '\0'; // Add a null terminator to the end of the password string
+    password[PASSWORD_LENGTH] = '\0'; /* Add a null terminator to the end of the password string */
 
     printf("Randomly generated password: %s\n", password);
 
