a16acc5887818212108f4fa2e893578492094e11
aliyu Abdulkabir <abdulkabiraliyu@gmail.com>
2023-03-27 13:27:09 -0700

    program that generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8
+
+int main() {
+    char password[PASSWORD_LENGTH+1];
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};:,.<>/?";
+
+    srand(time(NULL)); // Seed the random number generator with the current time
+
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = charset[rand() % (sizeof(charset) - 1)];
+    }
+    password[PASSWORD_LENGTH] = '\0'; // Add a null terminator to the end of the password string
+
+    printf("Randomly generated password: %s\n", password);
+
+    return 0;
+}
+
