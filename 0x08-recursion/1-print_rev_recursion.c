9ee17f66a44cfff0aefd8acfe5d8857b2607d757
abdulkabiraliyu <abdulkabiraliyu@gmail.com>
2023-09-30 01:48:09 -0700

    printing string in reverse order

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,2 +1,18 @@
 #include "main.h"
+/**
+ * _print_rev_recursion - this function prints string in reverse order
+ *
+ *@strn: input string
+ *
+ * Return: nothing is returned
+ */
 
+void _print_rev_recursion(char *strn)
+{
+
+	if (*strn != '\0')
+	{
+		_print_rev_recursion(strn + 1);
+		_putchar(*strn);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,6 @@
 
 char _putchar(char c);
 void _puts_recursion(char *strn);
-
+void _print_rev_recursion(char *strn);
 
 #endif /* MAIN_H */
