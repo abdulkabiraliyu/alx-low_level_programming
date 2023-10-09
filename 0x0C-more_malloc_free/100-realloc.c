1d5af8e7637013f4d0bf2f8478e25458d538eeff
abdulkabiraliyu <abdulkabiraliyu@gmail.com>
2023-10-08 21:45:30 -0700

    function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,6 +2,30 @@
 #include <string.h>
 
 /**
+* cp_memory - copy one poiter elements to another
+*
+* @ptr: pointer 1
+* @new_ptr: pointer 2
+* @size: size to be copied
+*
+* Return: pointer to the new copied
+*/
+
+void *cp_memory(void *ptr, void *new_ptr, unsigned int size)
+{
+	unsigned int i;
+	char *old = (char *) ptr;
+	char *new = (char *) new_ptr;
+
+	for (i = 0; i < size; i++)
+	{
+		new[i] = old[i];
+	}
+
+	return (new);
+}
+
+/**
 * _realloc - re allocate a memory space
 *
 * @ptr: pointer to old space
@@ -15,6 +39,10 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
+	unsigned int size;
+
+
+		size = old_size < new_size ? old_size : new_size;
 
 	if (ptr == NULL)
 	{
@@ -25,17 +53,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			free(new_ptr);
 			return (NULL);
 		}
+		new_ptr = cp_memory(ptr, new_ptr, size);
+		return (new_ptr);
 
 	}
 	if (new_size == old_size)
-	{
 		return (ptr);
-	}
+
 	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr)
+		free(ptr);
 		return (NULL);
-
 	}
 
 	new_ptr = malloc(new_size);
@@ -44,10 +72,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(new_ptr);
 		return (NULL);
 	}
-	else
-	{
-		memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+		new_ptr = cp_memory(ptr, new_ptr, size);
 		return (new_ptr);
 
-	}
+
 }
