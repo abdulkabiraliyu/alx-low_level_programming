6b2a049657942b8fc15a5a12969cc6de1509fb2b
abdulkabiraliyu <abdulkabiraliyu@gmail.com>
2023-10-10 00:41:25 -0700

    type of dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - poppy dog
+*
+* @name: member property
+* @age: member property
+* @owner: member property
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
