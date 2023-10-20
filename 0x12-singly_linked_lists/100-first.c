a83530599385d6fcd70be75e38d6cff6427e7479
abdulkabiraliyu <abdulkabiraliyu@gmail.com>
2023-10-20 08:51:58 -0700

    advanced task done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * print_before - execute before the main function
+  *
+  * Return: Nothing.
+  */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
